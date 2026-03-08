1119f6a032f8c98237e96b5e7677666b777f5e77
tawsifulMostafa <anasmoniriprof.787@gmail.com>
2026-03-09 04:02:05 +0600

    maybe 4th commit

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,58 @@ function handleLogin() {
     const userPassword = document.getElementById("passwordInput").value
     const errorMessage = document.getElementById('loginError')
     if (userName === 'admin' && userPassword === 'admin123') {
-        loginPage.classList.add('hidden') 
+        loginPage.classList.add('hidden')
         dashBoardPage.classList.remove('hidden')
-
-
+        loadIssues()
     } else {
         errorMessage.classList.remove('hidden')
     }
+
+
 }
+function loadIssues() {
+    fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
+        .then(result => result.json())
+        .then(data => {
+            const issues = data.data
+            renderCards(issues)
+        })
+}
+
+
+function renderCards(issue) {
+    const grid = document.getElementById('issues-Grid')
+    grid.classList.remove('hidden')
+    issue.forEach(issue => {
+        const card = document.createElement('div')
+        grid.appendChild(card)
+        card.className = `bg-white rounded-lg shadow-sm border-t-4 p-4`
+        card.innerHTML = `
+        <div>
+        <h3>${issue.title}</h3>
+        <p>${issue.description}</p>
+        <span>${issue.labels}</span>
+        <span>#${issue.id} ${issue.author}</span>
+        <span>${issue.priority}</span>
+        <span>${issue.createdAt}</span>
+        </div>
+        `
+        })
+    }
+
+    
+
+
+// "id": 1,
+//       "title": "Fix navigation menu on mobile devices",
+//       "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
+//       "status": "open",
+//       "labels": [
+//         "bug",
+//         "help wanted"
+//       ],
+//       "priority": "high",
+//       "author": "john_doe",
+//       "assignee": "jane_smith",
+//       "createdAt": "2024-01-15T10:30:00Z",
+//       "updatedAt": "2024-01-15T10:30:00Z
